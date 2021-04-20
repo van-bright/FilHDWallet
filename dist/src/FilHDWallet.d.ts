f249783e7fadbd650e1c7db119add21b916d4b14
Liangqin Fan <liangqin.fan@gmail.com>
2021-04-20 10:42:14 +0800

    submit to npm repository

---
+++ dist/src/FilHDWallet.d.ts
@@ -0,0 +1,14 @@
+import { MnemonicWalletProvider } from "passion-filecoin.js";
+import { KeyInfo } from "passion-filecoin.js/builds/dist/providers/Types";
+import { BigNumber } from 'bignumber.js';
+export declare class FilHDWallet extends MnemonicWalletProvider {
+    private mnemonic;
+    private con;
+    constructor(options: any, mnemonic?: string);
+    exportMnemonic(): string;
+    newAddress(): Promise<string>;
+    exportPrivateKey(account: string): Promise<KeyInfo>;
+    balanceOf(account: string): Promise<any>;
+    listTransactions(account: string, fromBlockHeight?: number): Promise<string>;
+    transfer(from: string, to: string, value: BigNumber, privateKey?: string): Promise<import("passion-filecoin.js/builds/dist/providers/Types").Cid>;
+}
